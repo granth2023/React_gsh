9b4a9ae17134f66be632da9725d134b27cc37723
granth2023 <grantharris38@gmail.com>
2024-02-20 17:54:33 -0500

    selected work imports

--- portfolio/src/components/SelectedWork.js
+++ portfolio/src/components/SelectedWork.js
@@ -1,1 +1,7 @@
 import React, { useRef, useEffect, useState} from 'react';
+import { useNavigate } from 'react-router-dom'
+// import { ReactComponent as SiteHyphen } from 'assests' 
+import { gsap } from 'gsap'
+import userEvent from '@testing-library/user-event'
+
+function 
