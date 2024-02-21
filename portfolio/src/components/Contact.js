aee28a3be01ff9aa540a79fe74084a56567ee2c5
granth2023 <grantharris38@gmail.com>
2024-02-21 14:24:53 -0500

    use effect for contact

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -6,3 +6,21 @@ function Contact({ modetoggle, pColor }) {
     const contactRef = useRef(null) 
     const [isInView, setIsInView] = useState(false)
     const hasAnimated = useRef(false)
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setIsInView(entry.isIntersecting)
+            },
+            {
+                root: null, 
+                rootMargin: '0px',
+                threshold: 0.5
+            }
+        )
+
+        if (contactRef.current) {
+            observer.observe(contactRef.current)
+        }
+        
+        )
