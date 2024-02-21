ad310b0efd6dc3dd1fb4a98a17e5757eae57cf8d
granth2023 <grantharris38@gmail.com>
2024-02-21 15:18:05 -0500

    landing imports

---
+++ portfolio/src/pages/Landing.js
@@ -0,0 +1,5 @@
+import React, { useRef, useEffect } from 'react';
+import SelectedWork from '../components/SelectedWork';
+import Contact from '../components/Contact';
+import { gsap } from 'gsap';
+
