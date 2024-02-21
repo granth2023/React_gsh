8069475602e924f24a81a3227eac483ca78d0581
granth2023 <grantharris38@gmail.com>
2024-02-21 14:30:13 -0500

    contact functionality

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -22,5 +22,33 @@ function Contact({ modetoggle, pColor }) {
         if (contactRef.current) {
             observer.observe(contactRef.current)
         }
+
+        return () => { 
+            if(contactRef.current) {
+                observer.unobserve(contactRef.current)
+            }
+        }
+    }, []);
         
+        useEffect(() => {
+            if (isInView && !hasAnimated.current) {
+                const elements = contactRef.current.children 
+                gsap.set(elements, { y:'-30%', autoAlpha: 0 })
+
+                gsap.to(elements, {
+                    y: '0%',
+                    autoAlpha: 1,
+                    stagger: 0.4,
+                    duration: 1,
+                    ease: 'power2.out',
+                    overwrite: 'auto'
+                })
+
+                hasAnimated.current = true
+            }
+        }, [isInView])
+
+        return ( 
+            
         )
+        })
