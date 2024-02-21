9c668cbef024e68c4a6cc850fe6b711e89a8b670
granth2023 <grantharris38@gmail.com>
2024-02-21 13:50:57 -0500

    if workref current observe it

--- portfolio/src/components/SelectedWork.js
+++ portfolio/src/components/SelectedWork.js
@@ -7,4 +7,24 @@ import userEvent from '@testing-library/user-event'
 function SelectedWork({ modeToggle, pColor, handleNavgiation}){
         const workRef = useRef(null);
         const [isInView, setIsInView] = useState(false);
+        const hasAnimated = useRef(false);
+
+        useEffect(() => {
+            const observer = new IntersectionObserver(
+                ([entry] => {
+                    setIsInView(entry.isIntersecting);
+                },
+                {
+                    root: null,
+                    rootMargin: '0px',
+                    threshold: 0.5,
+                }
+                );
+
+                if( workRef.current) {
+                    observer.observe(workRef.current);
+                }
+                
+            )
+        })
 }
