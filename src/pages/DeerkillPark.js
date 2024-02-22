7be205b115e418fe4294913ff0eec31719e0ce63
granth2023 <grantharris38@gmail.com>
2024-02-22 18:51:27 -0500

    return and export added

--- src/pages/DeerkillPark.js
+++ src/pages/DeerkillPark.js
@@ -23,6 +23,56 @@ function Deerkillpark( { modeToggle, pColor, handleNavigation}) {
     }, [])
 
     return (
-        
-    )
+        <div ref={childrenRef} style={{ color: pColor }} className="flex flex-col mx-6 mt-20 pt-16 sm:px-20 md:px-32 lg:px-48 xl:px-64 2xl:px-80 ">
+      <h2 className="text-5xl sm:text-8xl font-higuen">Crisol</h2>
+      <h3 className="text-lg sm:text-2xl font-aktiv font-bold mt-12">
+        Visual Identity
+      </h3>
+      <div className="flex justify-center mt-12">
+        <img
+          style={{ borderColor: pColor }}
+          className="border-[1px] w-fit h-auto"
+          src={crisol1}
+        />
+      </div>
+      <p className="font-aktiv mt-20 text-xs sm:text-sm lg:text-lg w-[80%]">
+      This identity was built out as a solution to a mock design prompt for a multi-purpose venue based in Santago, Chile. The name Crisol means "crucible", or the container in which components are smelted together. I wanted to invoke imagery from metallurgy or metalsmithing to communicate that the venue would be a place of high-energy experimentation.
+      </p>
+      <div className="flex justify-center mt-20">
+        <img
+          style={{ borderColor: pColor }}
+          className="border-[1px] w-fit h-auto"
+          src={crisol3}
+        />
+      </div>
+      <div className="flex justify-center mt-20">
+        <img
+          style={{ borderColor: pColor }}
+          className="border-[1px] w-fit h-auto"
+          src={crisol4}
+        />
+      </div>
+      <div className="flex justify-center mt-20">
+        <img
+          style={{ borderColor: pColor }}
+          className="border-[1px] w-fit h-auto"
+          src={crisol2}
+        />
+      </div>
+      <div>
+        <div className='flex flex-col font-higuen text-6xl mt-40 mb-40 justify-center items-center leading-snug'>
+          <div onClick={() => handleNavigation('/')} className='flex cursor-pointer'>
+          {modeToggle ? <SideArrow2 className='h-10 mt-4 mr-2'/> : <SideArrow className='h-10 mt-4 mr-2'/>}
+          <h2>Home&emsp;</h2>
+          </div>
+          <div onClick={() => handleNavigation('/vertrag')} className='flex cursor-pointer'>
+          <h2>&emsp;Next</h2>
+          {modeToggle ? <SideArrow2 className='h-10 mt-4 ml-2 rotate-180'/> : <SideArrow className='h-10 mt-4 ml-2 rotate-180'/>}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+   export default Deerkillpark 
