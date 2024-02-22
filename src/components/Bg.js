eb6a753b420233c9bccf332db0c7795e4a606a3d
granth2023 <grantharris38@gmail.com>
2024-02-22 13:11:10 -0500

    use effect ready to start

--- src/components/Bg.js
+++ src/components/Bg.js
@@ -1,4 +1,13 @@
 import React, { useState, useEffect } from 'react';
 //assest noise texture
 
-function 
+function Bg({ modeToggle }) {
+    const [hues, setHues] = useState([
+        10, 28, 50, 75, 42, 30, 10, 18, 35, 55, 63, 70
+    ])
+    const [directions, setDirections] = useState(new Array(12).fill(true))
+
+    useEffect(() => {
+        
+    }
+}
