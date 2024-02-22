0f61c6a50de3d2fa3bd24ffd00eb383bb630a40a
granth2023 <grantharris38@gmail.com>
2024-02-22 13:20:44 -0500

    gradient hue changes

--- src/components/Bg.js
+++ src/components/Bg.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import noiseTexture from '../assests/noiseTexture.png'
 //assest noise texture
 
 function Bg({ modeToggle }) {
@@ -79,6 +80,40 @@ function Bg({ modeToggle }) {
                }}
                />
                <div 
-               
+                id="gradientContainer"
+                className="h-full w-full"
+                style={{
+                  opacity: modeToggle ? 0 : 1,
+                  transition: 'ease-in-out all 0.4s',
+                  position: 'fixed',
+                  backgroundColor: `hsla(${hues[0]},40%,89%,1)`,
+                  backgroundImage:
+                  `radial-gradient(at 41% 50%, hsla(${
+                    hues[1]
+                  },100%,77%,1) 0px, transparent 50%),
+                  radial-gradient(at 29% 33%, hsla(${
+                  hues[2] + 220
+                  },100%,80%,1) 0px, transparent 50%),
+                  radial-gradient(at 32% 74%, hsla(${
+                  hues[3] + 220
+                  },100%,80%,1) 0px, transparent 50%),
+                  radial-gradient(at 77% 40%, hsla(${
+                  hues[4] + 220
+                  },100%,80%,1) 0px, transparent 50%),
+                  radial-gradient(at 65% 69%, hsla(${
+                  hues[5] + 220
+                  },100%,80%,1) 0px, transparent 50%),
+                  radial-gradient(at 50% 25%, hsla(${
+                  hues[6] + 220
+                  },100%,80%,1) 0px, transparent 50%),
+                  radial-gradient(at 90% 72%, hsla(${
+                  hues[7] + 220
+                  },100%,80%,1) 0px, transparent 50%),
+                  radial-gradient(at 20% 90%, hsla(${
+                  hues[8] + 220
+                  },100%,80%,1) 0px, transparent 50%)`
+                }}
+              />
+
         </div>
     )
