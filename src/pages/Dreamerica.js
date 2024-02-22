13c22a01c7611210256f1b016097f46b11b6149d
granth2023 <grantharris38@gmail.com>
2024-02-22 18:17:45 -0500

    function created

--- src/pages/Dreamerica.js
+++ src/pages/Dreamerica.js
@@ -2,3 +2,5 @@ import React. { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { gsap} from 'gsap';
+
+function Dreamerica ({ modeToggle, pColor, handleNavigation})
