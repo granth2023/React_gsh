ce33734a04778a80796df7c0e1551b7f552c64b7
granth2023 <grantharris38@gmail.com>
2024-02-22 18:19:52 -0500

    gsap set and to

--- src/pages/Dreamerica.js
+++ src/pages/Dreamerica.js
@@ -8,12 +8,15 @@ function Dreamerica ({ modeToggle, pColor, handleNavigation}) {
 
 
     useEffect(() => {
-        
-        )
+        const elements = childrenRef.current.children 
+        gsap.set(elements, { y:30, autoAlpha: 0 })
 
-
-
-
-
-}) 
+        gsap.to(elements, {
+            y: 0,
+            autoAlpha: 1,
+            stagger: 0.4,
+            duration: 1,
+            ease: 'easeInOut',
+            overwrite: 'auto'
+        })
 }, []);
