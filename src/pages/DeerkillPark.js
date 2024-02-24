3edc1578918171c0accf995c631a2fe2b46bb8ba
granth2023 <grantharris38@gmail.com>
2024-02-24 16:10:30 -0500

    dkp font and image

--- src/pages/DeerkillPark.js
+++ src/pages/DeerkillPark.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ReactComponent as SideArrow } from '../assests/sitesidearrow.svg'
 import { ReactComponent as SideArrow2 } from '../assests/sitesidearrow2.svg'
+import dkp from '../assests/dkp.webp'
 //side arrow 
 
 import { gsap } from 'gsap'
@@ -29,15 +30,15 @@ function Deerkillpark( { modeToggle, pColor, handleNavigation}) {
         ref={childrenRef} 
         style={{ color: pColor }} 
         className="flex flex-col mx-6 mt-20 pt-16 sm:px-20 md:px-32 lg:px-48 xl:px-64 2xl:px-80 ">
-      <h2 className="text-5xl text-center sm:text-8xl font-gothic">Deerkill Park</h2>
-      <h3 className="text-lg sm:text-2xl font-roadway font-bold mt-12">
-        Television, surreal, drama 
+      <h2 className="text-7xl text-center sm:text-8xl  lg:text-8xl xl:text-9xl  font-gothic">Deerkill Park</h2>
+      <h3 className="text-lg sm:text-5xl text-center font-gothic  font-bold italic mt-12">
+        Change is coming to Deerkill Park!  
       </h3>
       <div className="flex justify-center mt-12">
         <img
           style={{ borderColor: pColor }}
           className="border-[1px] w-fit h-auto"
-        //   src={crisol1}
+          src={dkp}
         />
       </div>
       <p className="font-roadway mt-20 text-xs sm:text-sm lg:text-lg w-[80%]">
