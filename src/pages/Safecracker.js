f89ee783dc8ed09dae7ebf4a8bbf00576340f0ac
granth2023 <grantharris38@gmail.com>
2024-02-24 14:26:03 -0500

    safecracker

--- src/pages/Safecracker.js
+++ src/pages/Safecracker.js
@@ -29,16 +29,34 @@ function Safecracker( { modeToggle, pColor, handleNavigation}) {
         ref={childrenRef} 
         style={{ color: pColor }} 
         className="flex flex-col mx-6 mt-20 pt-16 sm:px-20 md:px-32 lg:px-48 xl:px-64 2xl:px-80 ">
-      <h2 className="text-5xl sm:text-8xl font-gothic">Safecracker </h2>
-      <h3 className="text-lg sm:text-2xl font-roadway font-bold mt-12">
+      <h2 className="text-23xl text-center sm:text-8xl font-gothic">Safecracker </h2>
+      <h4 className="text-lg text-center sm:text-4xl font-gothic font-bold mt-12">
       There's money IN these (Hollywood) hills! 
-      </h3>
-      <h3 className="text-lg sm:text-2xl font-roadway font-bold mt-12">
+      </h4>
+      <h3 className="text-lg sm:text-2xl font-gothic font-bold mt-12">
+        Logline
+        </h3>
+      <h4 className="text-lg sm:text-2xl font-roadway font-bold mt-12">
       Believed to only be an old Hollywood fairytale, the bank vault underneath the Hollywood sign becomes the location for the worlds greatest safecracker CHARACTERs and his crew's biggest and final score. Is the vault even real? How will they get in? Who owns it? And will they make it out alive? 
-      </h3>
-      <h3 className="text-lg sm:text-2xl font-roadway font-bold mt-12">
-      In March of 2023 I heard an episode of the Bill Simmons podcast where him and Sean Fennessey did a bit coming up with a heist movie they would love to see John Bernthal star in. Without their knowledge, I decided to 
-      </h3>
+      </h4>
+      <h3 className="text-lg sm:text-2xl font-gothic font-bold mt-12">
+        Writer's Note
+        </h3>
+      <h4 className="text-lg sm:text-2xl font-roadway font-bold mt-12">
+      In March of 2023 I heard an episode of the Bill Simmons podcast where him and Sean Fennessey did a bit coming up with a heist movie they would love to see John Bernthal star in. Without their knowledge, I decided to take their 7 or so bullet points and use them as an outline for a spec script. 
+
+      Here were there ideas: 
+      * 
+      * 
+      * 
+      *
+      * 
+      * 
+
+      My big idea was that as someone from New Jersey striving for a career in the movies, I felt it was honest to make the crew from New Jersey and the L.A. bank be apart of the world of movies. 
+
+      I probably would have never thought of it without their idea and heist movies are always an easy watch for me so it was nothing but a pleasure to write it. I loved stepping outside some of my normal genre conventions and write in a genre that I've long admired. 
+      </h4>
 
       <div className="flex justify-center mt-12">
         <img
