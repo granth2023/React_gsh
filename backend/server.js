cc3d7075423bbc3a8a6b433ea60b482253ccded7
granth2023 <grantharris38@gmail.com>
2024-02-27 12:59:54 -0500

    post route for file upload

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,12 @@ import multer from 'multer';
 import Project from './models/Project.js';
 
 const app = express();
+const upload = multer({ dest: 'uploads/'});
 
+app.post('/projects/:projectId/files', upload.single('file', async (req, res) => {
+    try{
+        const 
+    }))
 
 
 
