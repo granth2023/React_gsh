4d5eba58d335210439ff80f0deb6d8dc25c86a18
granth2023 <grantharris38@gmail.com>
2024-02-28 00:40:03 -0500

    route

--- backend/routes.js
+++ backend/routes.js
@@ -1,2 +1,9 @@
 import express from 'express';
 import  { getProject } from './controllers/project';
+
+const router = express.Router();
+
+
+router.get('/projects/:projectId', getProject);
+
+export default router; 
