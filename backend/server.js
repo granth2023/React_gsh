61540b764ed82d07ae799a4b85a6e9bf9556faa9
granth2023 <grantharris38@gmail.com>
2024-02-28 01:08:37 -0500

    env import

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ import mongoose from 'mongoose';
 import multer from 'multer';
 import { GridFsStorage } from 'multer-gridfs-storage';
 import routes from './routes/routes.js'; // Adjust the path based on your structure
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const conn = mongoose.createConnection(process.env.MONGODB_URI);
