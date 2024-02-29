1389fd91fb2f62de17a79c2c007a3c3fdc50b336
granth2023 <grantharris38@gmail.com>
2024-02-28 21:40:26 -0500

    access protected file

--- backend/controllers.js
+++ backend/controllers.js
@@ -14,6 +14,28 @@ export const getProject = async (req, res) => {
     }
 };
 
+export const accessProtectedFile = async (req, res) => {
+    const { password, fileId } = req.body;
+
+    try {
+        const project = await Project.findOne({ "files._id": fileId}, { "files.$" : 1})
+
+        if(!project) {
+            return res.status(404).send('Project not found');
+        }
+
+        if(project.accessPassword && password === project.accessPassword) {
+            const file = project.files[0];
+            res.send({ url: file.url});
+        } else {
+            res.status(401).send('unanothrized or incorrect password')
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('server error')
+    }
+}
+
 const getFileStream = (gfs) => (req, res) => {
     gfs.find({ filename: req.params.filename}).toArray((err, files) => {
         if(!files || files.length === 0) {
