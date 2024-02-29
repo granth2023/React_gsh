9c3fe732daa93e7efea9939b60fdd68acda6779d
granth2023 <grantharris38@gmail.com>
2024-02-28 21:34:45 -0500

    password to project schema

--- backend/models.js
+++ backend/models.js
@@ -4,11 +4,13 @@ const fileSchema = new mongoose.Schema({
     filename: 'String',
     filepath: 'String',
     filetype: 'string',
+    url: 'string'
 });
 
 const projectSchema = new mongoose.Schema({
     name: {type: String, required: true},
     files: [fileSchema],
+    accessPassword: {type: String, required: true}
 });
 
 const Project = mongoose.model('Project', projectSchema)
