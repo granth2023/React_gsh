23275d9465f7d7ad219e78c3deee2f8e19b1a7de
granth2023 <grantharris38@gmail.com>
2024-03-12 11:43:03 -0400

    imports and const in funciton

--- src/pages/RadiowayDrive.js
+++ src/pages/RadiowayDrive.js
@@ -1,1 +1,16 @@
 import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import ReactModal from 'react-modal'
+// import { useNavigate } from 'react-router-dom'
+import { ReactComponent as SideArrow } from '../assests/sitesidearrow.svg'
+import { ReactComponent as SideArrow2 } from '../assests/sitesidearrow2.svg'
+
+import { gsap } from 'gsap'
+
+function radiowayDrive ( { modeToggle, pColor, handleNavigation}) {
+    const childrenRef = useRef(null)
+    const documentUrl = 'https://gshportfolio.s3.us-east-2.amazonaws.com/Radio+Driveway.MP4'
+
+
+
+}
