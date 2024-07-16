deff2e8e47407085b06f3874927e0901e4498cd5
granth2023 <grantharris38@gmail.com>
2024-07-15 21:21:55 -0400

    tailwind

---
+++ src/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+    darkMode: 'media', // or 'class' if you prefer
+  };
+  
