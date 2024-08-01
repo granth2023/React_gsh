2af72f7f1c45f8f055d1367a801d8c8f353ebdc1
granth2023 <grantharris38@gmail.com>
2024-07-31 21:14:31 -0400

    updated bio

--- src/components/SelectedWork.js
+++ src/components/SelectedWork.js
@@ -157,7 +157,7 @@ function SelectedWork({ pColor, handleNavigation, photoRef }) {
          A little about me: 
           <br />
           <br />
-        I live in Brooklyn, New York and my favorite genre to write is
+        I'm a cis bi jew living in Brooklyn, originalyl from New Jersey who loves movies and practices TM. My favorite genre to write is
         psychological mysteries.
         <br />
         <br />
