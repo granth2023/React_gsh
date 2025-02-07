198e825a8ea4c6e99df3686da28bb58d06948686
granth2023 <grantharris38@gmail.com>
2025-02-06 21:44:10 -0500

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,34 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
-  darkMode: 'media',
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: false,
-    theme: {
-      extend: {
-        keyframes: {
-          slowspin: {
-            '0%': { transform: 'rotate(0deg)' },
-            '100%': { transform: 'rotate(360deg)' },
-          },
-          updown: {
-            '0%': { transform: 'translateY(-5px)' },
-            '50%': { transform: 'translateY(15px)' }, 
-            '100%': { transform: 'translateY(-5px)' },
-          }
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}", "./public/index.html"],
+  darkMode: "media",
+  theme: {
+    extend: {
+      keyframes: {
+        slowspin: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
         },
-        animation: {
-          slowspin: 'slowspin 20s linear infinite',
-          updown: 'updown 2s ease-in-out infinite alternate'
-        },
-        fontFamily: {
-          'roadway': ['roadway', 'sans-serif'],
-          'gothic': ['gothic', 'serif'],
-          'SnellRoundhand': ['SnellRoundhand',  'serif'],
-          'SnellRoundhand-Bold': ['SnellRoundhand-Bold', 'serif']
-        },
-        fontWeight: {
-          'regular': 400,
-          'bold': 700,
+        updown: {
+          "0%": { transform: "translateY(-5px)" },
+          "50%": { transform: "translateY(15px)" },
+          "100%": { transform: "translateY(-5px)" },
         },
       },
+      animation: {
+        slowspin: "slowspin 20s linear infinite",
+        updown: "updown 2s ease-in-out infinite alternate",
+      },
+      fontFamily: {
+        roadway: ["roadway", "sans-serif"],
+        gothic: ["gothic", "serif"],
+        SnellRoundhand: ["Snell Roundhand", "serif"],
+        "SnellRoundhand-Bold": ["Snell Roundhand Bold", "serif"],
+      },
+      fontWeight: {
+        regular: 400,
+        bold: 700,
+      },
     },
-    variants: {},
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
