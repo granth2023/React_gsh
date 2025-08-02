61bf7310a132a48a2d62051e5c20fc7e05f93965
granth2023 <grantharris38@gmail.com>
2025-08-02 12:50:57 -0400

    updated

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
-import animatedBackground from "../components/animatedBackground.js";
+import AnimatedBackground from "../components/AnimatedBackground.js";
 
 
 import HamburgerNav from "../components/HamburgerNav.js";
@@ -33,8 +33,8 @@ function Services({ pColor, modeToggle, handleNavigation }) {
   }, []);
 
   return (
-    <div style={{ color: pColor }}>
-        <animatedBackground/>
+    <div className="text-xl sm:text-2xl leading-relaxed" style={{ color: pColor }}>
+        <AnimatedBackground/>
 
       <BgServices />
       <HamburgerNav handleNavigation={handleNavigation} />
