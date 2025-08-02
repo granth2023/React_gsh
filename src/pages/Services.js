5d15669ca915740d0b4a3e65ca3b1bd3b664cede
granth2023 <grantharris38@gmail.com>
2025-08-02 14:35:33 -0400

    updated

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -56,8 +56,8 @@ function Services({ pColor, modeToggle, handleNavigation }) {
         <div>
           <h3 className="text-2xl font-semibold mb-2">🧭 Mission</h3>
           <p className="text-xl leading-relaxed font-light tracking-wide">
-            It’s simple: serve the story. At every fork in the road, we ask what choice best serves the idea and story you want told. 
-            This ensures we stay true to the original core idea by always putting the story first so we can arrive at its truest, most fully realized form.
+            It’s simple: serve the story. At every fork in the road, we ask what choice best serves the story you want told. 
+            This ensures we stay true to your original core idea by always putting the story first so we can arrive at its truest, most fully realized form.
           </p>
         </div>
 
